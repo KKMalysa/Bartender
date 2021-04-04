5aeffbb62878e2d94867054217a43e3e16c8003d
karol <karol.malysa@interia.pl>
2021-04-04 21:47:35 +0200

    kosmetyczne poprawki

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,14 @@ public class Main {
         Drink forMe = alcoholDrinkFactory.makeAlcoholDrink(AlcoholDrinkType.POLISH_VODKA);
         Drink forMyFriend = alcoholDrinkFactory.makeAlcoholDrink(AlcoholDrinkType.DIRTY_HARRY);
 
+
+
         System.out.println("a drink for a driver contain : " + forDriver.getComponent1() + ", " + forDriver.getAmount1());
         System.out.println("a drink for me contain " + forMe.getComponent1() + ", " + forMe.getAmount1());
         System.out.println("a drink for my friend contain " + forMyFriend.getComponent1() + ", " + forMyFriend.getAmount1()
-                                                    + forMyFriend.getComponent2() + ", " + forMyFriend.getAmount2()
-                                                    + forMyFriend.getComponent3() + ", " + forMyFriend.getAmount3());
+                                                    + ", " + forMyFriend.getComponent2() + ", " + forMyFriend.getAmount2()
+                                                    + ", " + forMyFriend.getComponent3() + ", " + forMyFriend.getAmount3()
+                                                    + ". Calkowita pojemnosc drinka: " + (forMyFriend.getAmount1()+ forMyFriend.getAmount2()+ forMyFriend.getAmount3()));
 
 
 
